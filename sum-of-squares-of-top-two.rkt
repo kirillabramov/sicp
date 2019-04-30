16d58c1dab560cbbdc1b3af5d2f63d05312966fe
Kirill Abramov <40638034+kirillabramov@users.noreply.github.com>
2019-04-30 15:37:23 +0300

    update

--- sum-of-squares-of-top-two.rkt
+++ sum-of-squares-of-top-two.rkt
@@ -1,3 +1,7 @@
+#lang racket/base
+
+(require rackunit)
+
 ;; BEGIN (write your solution here)
 (define (sum-of-squares-of-top-two a b c)
     (define (square x) (* x x))
@@ -6,3 +10,7 @@
           (else (+ (square a) (square b))))
 )
 ;; END
+
+(check-equal? (sum-of-squares-of-top-two 1 2 3) 13)
+(check-equal? (sum-of-squares-of-top-two 3 4 2) 25)
+(check-equal? (sum-of-squares-of-top-two 5 3 4) 41)
